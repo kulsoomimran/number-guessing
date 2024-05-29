44ad228445904ec9cc8fdc799280587a1ffeeb2c
Kulsoom Imran <kulsoomimran1901@gmail.com>
2024-05-30 01:44:41 +0500

    This is a number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        message: "Please guess a number between 1-6: ",
+        type: "number",
+        name: "guessedNumber",
+    },
+]);
+if (answers.guessedNumber === randomNumber) {
+    console.log("CONGRATULATIONS!!! You guessed the right number");
+}
+else {
+    console.log("You guessed the wrong number");
+}
